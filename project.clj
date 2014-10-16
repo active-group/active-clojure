7fec97d10eb3616f1b9a3ae21bdf7c72bb8f69c0
Marcus Crestani <marcus@crestani.de>
2014-10-16 10:04:49 +0200

    Run non-cljx tests as well.

--- project.clj
+++ project.clj
@@ -4,15 +4,15 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.6.0"]
-                 [org.clojure/clojurescript "0.0-2280" :scope "provided"]]
+                 [org.clojure/clojurescript "0.0-2371"]]
 
-  :plugins [;; NB: The :exclusions argument quiets version-ranges warning.
+  :plugins [;; NB: The :exclusions argument quiets some version-ranges warning.
             [com.keminglabs/cljx "0.4.0" :exclusions [org.clojure/clojure]]
             [lein-cljsbuild "1.0.3"]
             ;; NB: This needs a version of clojurescript.test with the Nashorn runner,
             ;; for example from the nashorn-runner branch from
             ;; https://github.com/active-group/clojurescript.test
-            ;; The :exclusions argument quiets version-ranges warning.
+            ;; The :exclusions argument quiets some version-ranges warning.
             [com.cemerick/clojurescript.test "0.3.2-SNAPSHOT"  :exclusions [org.clojure/clojure]]
             [org.bodil/lein-nashorn "0.1.2"]]
 
@@ -31,15 +31,17 @@
                   {:source-paths ["test/cljx"]
                    :output-path "target/generated/test/cljs"
                    :rules :cljs}]}
-  :source-paths ["target/generated/src"]
+  :source-paths ["src"
+                 "target/generated/src"]
 
-  :test-paths ["target/generated/test/clj"]
+  :test-paths ["test" 
+               "target/generated/test/clj"]
 
   :cljsbuild {:builds
               {:dev {:source-paths ["target/classes"]
-                             :compiler {:output-to "target/main.js"
-                                        :optimizations :whitespace
-                                        :pretty-print true}}
+                     :compiler {:output-to "target/main.js"
+                                :optimizations :whitespace
+                                :pretty-print true}}
                :test {:source-paths ["target/generated/src"
                                      "target/generated/test/cljs"]
                       :compiler {:output-to "target/test.js"

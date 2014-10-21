fef5c346b0825a332fb4772aa063dbee863391b7
Marcus Crestani <marcus.crestani@active-group.de>
2014-10-21 20:32:33 +0200

    Next dev cycle.

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject active-clojure "0.2.0"
+(defproject active-clojure "0.3.0-SNAPSHOT"
   :description "Active Clojure: Various Clojure utilities in use at Active Group"
   :url "http://github.com/active-group/reacl/active-group"
   :license {:name "Eclipse Public License"

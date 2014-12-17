f681645cce4e86af903fe4f3223628e0e78bd4a9
Mike Sperber <sperber@deinprogramm.de>
2014-12-17 15:34:06 +0100

    Fix github URL.

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject active-clojure "0.4.0-SNAPSHOT"
   :description "Active Clojure: Various Clojure utilities in use at Active Group"
-  :url "http://github.com/active-group/reacl/active-group"
+  :url "http://github.com/active-group/active-clojure"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.6.0"]

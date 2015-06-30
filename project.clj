f59048ced1c45339cb49818b446468e6e9886107
Mike Sperber <sperber@deinprogramm.de>
2015-06-30 17:42:04 +0200

    Clojure 1.7.0.

--- project.clj
+++ project.clj
@@ -3,7 +3,7 @@
   :url "http://github.com/active-group/active-clojure"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.7.0-RC1"]
+  :dependencies [[org.clojure/clojure "1.7.0"]
                  [org.clojure/clojurescript "0.0-3308"]]
 
   :generated-paths ["target"]

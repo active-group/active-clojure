339895d65321bbba5e98b75777eeee5f89d7adcb
Mike Sperber <sperber@deinprogramm.de>
2015-07-09 14:42:51 +0200

    Add install-default-uncaught-exception-handler!.
    Installs an exception handler of last resort that will print a stack
    trace.

--- src/active/clojure/condition_hooks.clj
+++ src/active/clojure/condition_hooks.clj
@@ -35,3 +35,43 @@
 
 (defn activate-clojure-test! []
   @activation-body)
+
+(defn root-cause
+  "Returns the initial cause of an exception or error by peeling off all of
+  its wrappers"
+  [^Throwable t]
+  (loop [cause t]
+    (if (and (instance? clojure.lang.Compiler$CompilerException cause)
+             (not= (.source ^clojure.lang.Compiler$CompilerException cause) "NO_SOURCE_FILE"))
+      (.getCause cause)) ; one level up ...
+      (if-let [cause (.getCause cause)]
+        (recur cause)
+        cause)))
+
+(defn print-stack-trace-of
+  [^Throwable exc]
+  (let [st (.getStackTrace exc)]
+    (if-let [e (first st)]
+      (clojure.stacktrace/print-trace-element e)
+      (print "[empty stack trace]"))
+    (newline)
+    (doseq [e (rest st)]
+      (print " ")
+      (clojure.stacktrace/print-trace-element e)
+      (newline))))
+
+(defn install-default-uncaught-exception-handler!
+  "Install an exception handler of last resort that will print a stack trace."
+  []
+  (Thread/setDefaultUncaughtExceptionHandler
+   (reify Thread$UncaughtExceptionHandler
+     (uncaughtException [this t e0]
+       (let [e (root-cause e0)]
+         (if (c/condition? e)
+           (do
+             (c/print-condition e *err*)
+             (.flush ^java.io.PrintWriter *err*))
+           (binding [*out* *err*]
+             (println (.getSimpleName (class e)) (.getMessage ^Exception e))
+             (print-stack-trace-of e)
+             (.flush ^java.io.PrintWriter *err*))))))))

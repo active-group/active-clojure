a606631c48586d8eacbfcfb98bb5ede83b143c59
Mike Sperber <sperber@deinprogramm.de>
2015-07-23 15:17:09 +0200

    0.11.0 release.

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject active-clojure "0.10.0"
+(defproject active-clojure "0.11.0"
   :description "Active Clojure: Various Clojure utilities in use at Active Group"
   :url "http://github.com/active-group/active-clojure"
   :license {:name "Eclipse Public License"

963b609ab0a3f5e20f3297936dd31a624e692e0f
Mike Sperber <sperber@deinprogramm.de>
2016-03-18 17:47:02 +0100

    Add active.clojure.macro.

---
+++ src/active/clojure/macro.clj
@@ -0,0 +1,15 @@
+(ns active.clojure.macro
+  "Macro helpers.")
+
+;; Repeated in numerous projects
+
+(clojure.core/defn cljs-env?
+  "Take the &env from a macro, and tell whether we are expanding into cljs."
+  [env]
+  (boolean (:ns env)))
+
+(defmacro if-cljs
+  "Return then if we are generating cljs code and else for Clojure code.
+   https://groups.google.com/d/msg/clojurescript/iBY5HaQda4A/w1lAQi9_AwsJ"
+  [then else]
+  (if (cljs-env? &env) then else))

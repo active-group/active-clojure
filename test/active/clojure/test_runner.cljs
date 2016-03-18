430460b9da3efec1ba33860eeb3fd211be5100d7
Mike Sperber <sperber@deinprogramm.de>
2016-03-18 17:48:00 +0100

    Missing test runner for doo.

---
+++ test/active/clojure/test_runner.cljs
@@ -0,0 +1,13 @@
+(ns active.clojure.test-runner
+  (:require [doo.runner :refer-macros [doo-tests]]
+            [active.clojure.condition-test]
+            [active.clojure.debug-test]
+            [active.clojure.lens-test]
+            [active.clojure.monad-test]
+            [active.clojure.record-test]))
+
+(doo-tests 'active.clojure.condition-test
+           'active.clojure.debug-test
+           'active.clojure.lens-test
+           'active.clojure.monad-test
+           'active.clojure.record-test)

1018ddb74e167990103d64aaaa3bd20f2379eaa7
Mike Sperber <sperber@deinprogramm.de>
2016-12-09 16:44:18 +0100

    Only make ClojureScript a dependency in :cljs profile

--- project.clj
+++ project.clj
@@ -4,7 +4,6 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.8.0"]
-                 [org.clojure/clojurescript "1.9.293"]
                  [io.aviso/pretty "0.1.24"]]
 
   :generated-paths ["target"]
@@ -26,7 +25,8 @@
                                  :optimizations :whitespace
                                  :pretty-print true}}}}
 
-  :profiles {:dev {:dependencies [[lein-doo "0.1.6"]]}}
+  :profiles {:dev {:dependencies [[lein-doo "0.1.6"]]}
+             :cljs {:dependencies [[org.clojure/clojurescript "1.9.293"]]}}
 
   :aliases {"test-nashorn" ["doo" "nashorn" "test"]}
 

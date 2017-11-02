fcb40bf339033f926e1912e9ca8b7872e8db10ad
Mike Sperber <sperber@deinprogramm.de>
2017-11-02 19:54:04 +0100

    0.21.1 release

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject active-clojure "0.22.0-SNAPSHOT"
+(defproject active-clojure "0.21.1"
   :description "Active Clojure: Various Clojure utilities in use at Active Group"
   :url "http://github.com/active-group/active-clojure"
   :license {:name "Eclipse Public License"

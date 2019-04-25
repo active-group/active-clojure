ed62d92e7ab13a90ab9ec1e7d5dce7fcf9b1108e
Mike Sperber <sperber@deinprogramm.de>
2019-04-25 09:55:41 +0200

    Rudimentary runtime support for record w/o classes.

---
+++ src/active/clojure/record_runtime.clj
@@ -0,0 +1,126 @@
+(ns active.clojure.record-runtime
+  (:refer-clojure :exclude [record?]))
+
+(defrecord RecordField [name])
+
+(defn make-record-field
+  [name]
+  (RecordField. name))
+
+(defrecord RecordTypeDescriptor [name uid fields])
+
+(defn make-record-type-descriptor
+  [name uid fields]
+  (RecordTypeDescriptor. name uid (to-array fields)))
+
+(defn record-type-descriptor?
+  [x]
+  (instance? RecordTypeDescriptor x))
+
+(defn record-type-descriptor-field-index
+  [^RecordTypeDescriptor rtd name]
+  (loop [i 0
+         fields (.-fields rtd)]
+    (if (empty? fields)
+      ;; FIXME: more expressive exception
+      (throw (new Error (str "field " name "not found in rtd " (.-name rtd))))
+      (let [^RecordField field (first fields)]
+        (if (identical? (.-name field) name)
+          i
+          (recur (inc i)
+                 (rest fields)))))))
+          
+(deftype Record
+    [^RecordTypeDescriptor rtd
+     ^{:tag "[Ljava.lang.Object;"} slots])
+
+(defmacro really-make-record
+  [rtd & vs]
+  (let [a `a#]
+    `(let [~a (object-array ~(count vs))]
+       ~@(map-indexed (fn [i v]
+                        `(aset ~a ~i ~v))
+                      vs)
+       (Record. ~rtd ~a))))
+
+(defn make-record
+  ([^RecordTypeDescriptor rtd]
+   (Record. rtd (object-array 0)))
+  ([^RecordTypeDescriptor rtd v0]
+   (really-make-record rtd v0))
+  ([^RecordTypeDescriptor rtd v0 v1]
+   (really-make-record rtd v0 v1))
+  ([^RecordTypeDescriptor rtd v0 v1 v2]
+   (really-make-record rtd v0 v1 v2))
+  ([^RecordTypeDescriptor rtd v0 v1 v2 v3]
+   (really-make-record rtd v0 v1 v2 v3))
+  ([^RecordTypeDescriptor rtd v0 v1 v2 v3 v4]
+   (really-make-record rtd v0 v1 v2 v3 v4))
+  ([^RecordTypeDescriptor rtd v0 v1 v2 v3 v4 v5]
+   (really-make-record rtd v0 v1 v2 v3 v4 v5))
+  ([^RecordTypeDescriptor rtd v0 v1 v2 v3 v4 v5 v6]
+   (really-make-record rtd v0 v1 v2 v3 v4 v5))
+  ([^RecordTypeDescriptor rtd v0 v1 v2 v3 v4 v5 v6 v7]
+   (really-make-record rtd v0 v1 v2 v3 v4 v5 v6 v7))
+  ([^RecordTypeDescriptor rtd v0 v1 v2 v3 v4 v5 v6 v7 v8]
+   (really-make-record rtd v0 v1 v2 v3 v4 v5 v6 v7 v8))
+  ([^RecordTypeDescriptor rtd v0 v1 v2 v3 v4 v5 v6 v7 v8 v9]
+   (really-make-record rtd v0 v1 v2 v3 v4 v5 v6 v7 v8 v9))
+  ;; FIXME: more
+  ([^RecordTypeDescriptor rtd v0 v1 v2 v3 v4 v5 v6 v7 v8 v9 v10 & vs]
+   (let [a (object-array (+ 11 (count vs)))]
+     (aset a 0 v0)
+     (aset a 1 v1)
+     (aset a 2 v2)
+     (aset a 3 v3)
+     (aset a 4 v4)
+     (aset a 5 v5)
+     (aset a 6 v6)
+     (aset a 7 v7)
+     (aset a 8 v8)
+     (aset a 9 v9)
+     (aset a 10 v10)
+     (loop [vs vs
+            i 11]
+       (when-not (empty? vs)
+         (aset a i (first vs))
+         (recur (rest vs) (inc i))))
+     (Record. rtd a))))
+   
+(defn record?
+  [x]
+  (instance? Record x))
+
+(defn record-rtd
+  [^Record r]
+  (.-rtd r))
+
+(defn record-of-type?
+  [^Record r ^RecordTypeDescriptor rtd]
+  (and (record? r)
+       (identical? rtd (.-rtd r))))
+
+; assumes that ?r, ?rtd are identifiers alerady
+(defmacro record-check-rtd!
+  [?rtd ?r]
+  `(when-not (and (record? ~?r)
+                  (identical? ~?rtd (.-rtd ~?r)))
+     ;; FIXME: more expressive exception
+     (throw (new Error "not a record of the correct type"))))
+
+(defn record-get
+  [^RecordTypeDescriptor rtd ^Record r ^long index]
+  (record-check-rtd! rtd r)
+  (aget ^{:tag "[Ljava.lang.Object;"} (.-slots r) index))
+  
+(defn record-update
+  [^RecordTypeDescriptor rtd ^Record r ^long index v]
+  (record-check-rtd! rtd r)
+  (let [slots
+        (aclone ^{:tag "[Ljava.lang.Object;"} (.-slots r))]
+    (aset slots index v)
+    (Record. (.-rtd r) slots)))
+
+;; FIXME: lens
+;; FIXME: serialization   
+
---
+++ test/active/clojure/record_runtime_test.clj
@@ -0,0 +1,43 @@
+(ns active.clojure.record-runtime-test
+    (:require [active.clojure.record-runtime :as r]
+              #?(:clj [clojure.test :refer :all])
+              #?(:cljs [cljs.test :as t]))
+    #?(:cljs
+       (:require-macros [cljs.test
+                         :refer (is deftest run-tests testing)])))
+
+(def rtd0 (r/make-record-type-descriptor `rtd0 nil []))
+(def rtd1 (r/make-record-type-descriptor `rtd1 nil [(r/make-record-field "f1")]))
+(def rtd2 (r/make-record-type-descriptor `rtd2 nil [(r/make-record-field "f1")
+                                                    (r/make-record-field "f2")]))
+(def rtd100 (r/make-record-type-descriptor `rtd100 nil
+                                           (map #(str "f" %) (range 100))))
+
+(deftest t-make-record-simple
+  (let [r0 (r/make-record rtd0)
+        r1 (r/make-record rtd1 1)
+        r2 (r/make-record rtd2 1 2)]
+    (is (r/record? r0))
+    (is (r/record-of-type? r0 rtd0))
+    (is (not (r/record-of-type? r0 rtd1)))
+    (is (r/record? r1))
+    (is (= 1 (r/record-get rtd1 r1 0)))
+    (is (r/record? r2))
+    (is (= 1 (r/record-get rtd2 r2 0)))
+    (is (= 2 (r/record-get rtd2 r2 1)))))
+
+(deftest t-make-record-100
+  (let [r100 (apply r/make-record rtd100 (range 100))]
+    (doseq [i (range 100)]
+      (is (= i (r/record-get rtd100 r100 i))))))
+
+(deftest t-record-update
+  (let [r2 (r/make-record rtd2 1 2)
+        r2a (r/record-update rtd2 r2 1 5)]
+    (is (= 1 (r/record-get rtd2 r2a 0)))
+    (is (= 5 (r/record-get rtd2 r2a 1)))))
+
+(deftest t-record-check
+  (let [r2 (r/make-record rtd2 1 2)]
+    (is (thrown? Error (r/record-get rtd0 r2 0)))
+    (is (thrown? Error (r/record-update rtd0 r2 0 :new)))))

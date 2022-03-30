46de27c13075824c527037561d08312a7ba503b7
Marco Schneider <marco.schneider@active-group.de>
2022-03-30 10:23:50 +0200

    Bump io.aviso/pretty to version 1.1.1.

--- project.clj
+++ project.clj
@@ -5,7 +5,7 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.10.0" :scope "provided"]
                  [org.clojure/clojurescript "1.10.238" :scope "provided"]
-                 [io.aviso/pretty "0.1.34"]
+                 [io.aviso/pretty "1.1.1"]
                  [org.clojure/core.match "1.0.0"]
                  [org.clojure/test.check "0.10.0-alpha4"]]
 

03abef91fa61a1aaabdf2a6ef9a36a8cd27da07e
Johannes Maier <johannes.maier@active-group.de>
2025-04-02 14:37:48 +0200

    dynj: integrate with clj-kondo

---
+++ resources/clj-kondo.exports/de.active-group/active-clojure/hooks/dynj.clj
@@ -0,0 +1,10 @@
+(ns hooks.dynj
+  (:require [clj-kondo.hooks-api :as api]))
+
+(defn declare-dynj
+  [{:keys [node]}]
+  (let [[name second & more] (rest (:children node))
+        new-node (api/list-node
+                  (list (api/token-node 'do)
+                        (api/list-node [(api/token-node 'declare) name])))]
+    {:node new-node}))

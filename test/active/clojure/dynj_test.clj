15d6425854366431f1a34a9b45ad7a4535c75fb2
Johannes Maier <johannes.maier@active-group.de>
2025-04-10 10:15:34 +0200

    dynj: test `bind-fn*`

--- test/active/clojure/dynj_test.clj
+++ test/active/clojure/dynj_test.clj
@@ -19,7 +19,7 @@
   (dynj/binding [foo (fn [x] (* x 2))]
     (t/is (= 8 @(future (foo 4))))))
 
-(t/deftest bound-fn-test
+(t/deftest bound-fn*-test
   ;; bind a function to current bindings
   (let [f (dynj/binding [foo (fn [x] (* x 2))]
             (dynj/bound-fn* (fn [v]
@@ -29,6 +29,12 @@
   (let [f (dynj/binding [foo (fn [x] (* x 2))]
             (fn [v] (foo v)))]
     (t/is (thrown? Exception (f 4)))))
+
+(t/deftest bind-fn*-test
+  (let [bindings {#'foo #'str}
+        f (fn [x] (foo (inc x)))
+        bound (dynj/bind-fn* bindings f)]
+    (t/is (= "5" (bound 4)))))
 
 (t/deftest with-bindings*-test
   (t/is

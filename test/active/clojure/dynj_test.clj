ad1811ee3fcb30569cf97050237d4353e00eca06
Johannes Maier <johannes.maier@active-group.de>
2025-04-10 09:42:23 +0200

    dynj-test: drop 'sut' idiom
    It's not used in any other tests in this project.

--- test/active/clojure/dynj_test.clj
+++ test/active/clojure/dynj_test.clj
@@ -1,8 +1,8 @@
 (ns active.clojure.dynj-test
-  (:require [active.clojure.dynj :as sut]
+  (:require [active.clojure.dynj :as dynj]
             [clojure.test :as t]))
 
-(sut/declare-dynj foo "Foo" [bar])
+(dynj/declare-dynj foo "Foo" [bar])
 
 (t/deftest declare-dynj-test
   ;; (t/is (:dynamic (meta #'foo)))
@@ -11,31 +11,31 @@
 (t/deftest binding-test
   (t/is (thrown? Exception (foo 4)))
   
-  (sut/binding [foo (fn [x] (* x 2))]
+  (dynj/binding [foo (fn [x] (* x 2))]
     (t/is (= 8 (foo 4)))))
 
 (t/deftest threading-test
   ;; threads 'inherit' current bindings
-  (sut/binding [foo (fn [x] (* x 2))]
+  (dynj/binding [foo (fn [x] (* x 2))]
     (t/is (= 8 @(future (foo 4))))))
 
 (t/deftest bound-fn-test
   ;; bind a function to current bindings
-  (let [f (sut/binding [foo (fn [x] (* x 2))]
-            (sut/bound-fn* (fn [v]
-                             (foo v))))]
+  (let [f (dynj/binding [foo (fn [x] (* x 2))]
+            (dynj/bound-fn* (fn [v]
+                              (foo v))))]
     (t/is (= 8 (f 4)))))
 
 (t/deftest with-bindings*-test
   (t/is
    (= 7
-      (sut/with-bindings*
+      (dynj/with-bindings*
         ;; just "foo" doesn't work here; Var is expected
         {#'foo (fn [x] (+ x 4))}
         ;; expects a thunk
         (fn [] (foo 3))))))
 
-(sut/defn-dynj bar [arg]
+(dynj/defn-dynj bar [arg]
   (* 3 arg))
 
 (t/deftest defn-dynj-default-implementation-test
